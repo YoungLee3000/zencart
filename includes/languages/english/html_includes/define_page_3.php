cc6d05c0276431f5ce8cbcb26884d69a5317adb6
unknown <liyong.xmu@qq.com>
2019-11-30 20:51:58 +0800

    定义付款成功页面

--- includes/languages/english/html_includes/define_page_3.php
+++
@@ -1,3 +0,0 @@
-<p><strong>Exemple de texte pour la Page 3 ...</strong></p>
-<p>Ce texte peut être modifié dans l'éditeur des pages 'define' situé dans le menu Outils de l'Administration.</p>
-<p>Pour enlever ce texte, supprimez le avec ce même éditeur de pages.</p>
